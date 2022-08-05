891d75bd7e685e068fa53d12a5f1e666c81c4767
Saddique <saddique_uet@yahoo.com>
2022-08-05 15:42:17 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,6 +2,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 3.36"    # This "~>" sign will help to upgrade the version from 3.36 to the latest version like 3.75 
+                             # but if you user 3.36.0, it will update to 3.36.latest version.
     }
   }
 }
